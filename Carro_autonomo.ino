4a1c6981cf2803427469caef51abead545c436ac
SantosPereira <pedrohenriquelemam@gmail.com>
2021-04-16 18:38:25 -0300

    Add controle dos motores

--- Carro_autonomo.ino
+++ Carro_autonomo.ino
@@ -9,6 +9,11 @@
 // Define a porta 8 como entrada e saída de dados
 const int porta = 8;
 
+const int motor1 = 10; // motor traseiro da esquerda na perspectiva do motorista
+const int motor2 = 11; // motor traseiro direto
+const int motor3 = 12; // motor frontal esquerdo
+const int motor4 = 13; // motor frontal direito
+
 void setup() {
   // Ecoa a distância medida na porta serial
   Serial.begin(9600);
@@ -16,7 +21,17 @@ void setup() {
 
 void loop() {
   // Definição das variáveis
+  char direcao;
   long duracao, cm;
+
+  direcao = 'front';
+
+  //          Opções
+  //'front'
+  //'stop'
+  //'back'
+  //'right'
+  //'left'
 
   // O sensor vai emitir um pulso de 2 ou mais microsegundos.
   pinMode(porta, OUTPUT);
@@ -37,6 +52,76 @@ void loop() {
   Serial.println();
 
   delay(100);
+  /////////////////////////////
+
+  // Faz desviar de obstáculos virando sempre a direita
+  if (cm < 10)
+  {
+    direcao = 'right';
+  }
+
+  // Controle dos motores
+  if (direcao == 'front')
+  {
+    pinMode(motor1, OUTPUT);
+    digitalWrite(motor1, HIGH);
+    
+    pinMode(motor2, OUTPUT);
+    digitalWrite(motor2, HIGH);
+    
+    pinMode(motor3, OUTPUT);
+    digitalWrite(motor3, HIGH);
+    
+    pinMode(motor4, OUTPUT);
+    digitalWrite(motor4, HIGH);
+  }
+  if (direcao == 'back')
+  {
+
+  }
+    if (direcao == 'stop')
+  {
+    pinMode(motor1, OUTPUT);
+    digitalWrite(motor1, LOW);
+  
+    pinMode(motor2, OUTPUT);
+    digitalWrite(motor2, LOW);
+  
+    pinMode(motor3, OUTPUT);
+    digitalWrite(motor3, LOW);
+  
+    pinMode(motor4, OUTPUT);
+    digitalWrite(motor4, LOW);
+  }
+    if (direcao == 'right')
+  {
+    pinMode(motor1, OUTPUT);
+    digitalWrite(motor1, HIGH);
+  
+    pinMode(motor2, OUTPUT);
+    digitalWrite(motor2, LOW);
+  
+    pinMode(motor3, OUTPUT);
+    digitalWrite(motor3, HIGH);
+  
+    pinMode(motor4, OUTPUT);
+    digitalWrite(motor4, LOW);
+  }
+    if (direcao == 'left')
+  {
+    pinMode(motor1, OUTPUT);
+    digitalWrite(motor1, LOW);
+  
+    pinMode(motor2, OUTPUT);
+    digitalWrite(motor2, HIGH);
+  
+    pinMode(motor3, OUTPUT);
+    digitalWrite(motor3, LOW);
+  
+    pinMode(motor4, OUTPUT);
+    digitalWrite(motor4, HIGH);
+  }
+  ///////////////////////////////
 }
 
 long msPARAcm(long ms) {
